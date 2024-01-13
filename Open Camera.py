db830df6332d342e3a1e5ed49878ede137cca881
Athul-mk <athulmk00@gmail.com>
2024-01-13 12:04:49 +0530

    Open Cv2

---
+++ Open Camera.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+cap=cv2.VideoCapture('http://192.168.137.111:4747/video')
+cpt=0
+while(True):
+   ret,frame=cap.read()
+   faces=eye_cascade.detectMultiScale(frame,1.2,4)
+   for(x,y,w,h) in faces:
+      crop=frame[y:y+h,x:x+w]
+      cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+   cv2.imshow("frame",frame)
+   cpt+=1
+   k=cv2.waitKey(1)&0xFF
+   if k==ord('q'):
+            break
+
+
